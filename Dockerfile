e4ec4e3b53cbd71215eacf3f7ff5447883e76fea
paoloriccardi <p.riccardi@gmail.com>
2021-03-31 21:36:19 +0200

    changed main app name

--- Dockerfile
+++ Dockerfile
@@ -5,4 +5,4 @@ WORKDIR /app
 RUN pip install -r requirements.txt
 EXPOSE 5001
 ENTRYPOINT [ "python" ]
-CMD [ "app.py" ]
+CMD [ "appGateway.py" ]
--- appGateway.py
+++ appGateway.py
@@ -24,4 +24,4 @@ def write():
 
 
 if __name__ == "__main__":
-    app.run(debug=True, host='0.0.0.0', port=5001)
+    app.run(debug=True, host='0.0.0.0', port=7001)
